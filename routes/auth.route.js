4ac0932dd1b8068325119e1b496f139430c40dd2
21MoududAhammed <21moudud@gmail.com>
2024-09-02 19:01:10 +0600

    Login process done with generating jwt token

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userModel = require("../models/user.model.js");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // to register
 router.post("/register", async (req, res) => {
@@ -15,7 +16,7 @@ router.post("/register", async (req, res) => {
       userName,
       password: hashPassword,
     });
-    //  destructure to ignore password to send 
+    //  destructure to ignore password to send
     const { password: _, ...others } = result.toObject();
 
     return res
@@ -23,13 +24,33 @@ router.post("/register", async (req, res) => {
       .json({ message: "Successfully create one user", data: others });
   } catch (err) {
     console.log(err);
-    res.status(501).json({message: 'Server side error', error: err?.message})
+    res.status(501).json({ message: "Server side error", error: err?.message });
   }
 });
 
-// to login 
-router.post("/login", async(req, res)=>{
-    
-})
+// to login
+router.post("/login", async (req, res) => {
+  const { userName,name,password } = req.body;
+  try {
+    const user = await userModel.findOne({ userName: userName });
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      res.status(401).json({ message: "Authentication Failed." });
+    }
+
+    const token = jwt.sign({ userName, name }, process.env.JWT_PRIVATE_KEY);
+
+    res
+      .status(200)
+      .json({ message: "Logged in successfully", jwt_token: token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: `Server side error: ${err?.message}` });
+  }
+});
 
 module.exports = router;
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,4 +1,4 @@
 // const express = require('express');
 // const router = express.Router();
 
-// router.get('/',(res))
+// router.get('/', (res, ))
